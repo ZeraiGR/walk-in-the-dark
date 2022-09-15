998248dcbef78ae4ebbb9abe7843e3433e5b70d3
ZeraiGR <kirillgr15@mail.ru>
2022-09-15 21:16:57 +0300

    implemented guide

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,17 +3,29 @@
 // import { formsModules } from "./forms/forms.js";
 
 export function headerFixed() {
-    const header = document.querySelector('.header'),
-        windowHeight = document.documentElement.clientHeight,
-        arrowTopBtn = document.querySelector('.arrow-top');
+  const header = document.querySelector('.header'),
+    windowHeight = document.documentElement.clientHeight,
+    arrowTopBtn = document.querySelector('.arrow-top');
 
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > windowHeight) {
-            header.classList.add('fixed');
-            arrowTopBtn.classList.add('active');
-        } else {
-            header.classList.remove('fixed');
-            arrowTopBtn.classList.remove('active');
-        }
-    });
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > windowHeight) {
+      header.classList.add('fixed');
+      arrowTopBtn.classList.add('active');
+    } else {
+      header.classList.remove('fixed');
+      arrowTopBtn.classList.remove('active');
+    }
+  });
 }
+
+const soundBtnsHandler = () => {
+  const soundBtns = document.querySelectorAll('.sound-button');
+
+  soundBtns?.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      btn.classList.toggle('active');
+    });
+  });
+};
+
+soundBtnsHandler();
