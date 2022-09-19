08c4e08d07e508289a5c76a56d54513cc7d812c6
ZeraiGR <kirillgr15@mail.ru>
2022-09-19 18:49:06 +0300

    implemented about section

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -1,4 +1,3 @@
-
 /*
 Документация по работе в шаблоне: https://www.lightgalleryjs.com/docs/
 Документация плагина: https://www.lightgalleryjs.com/docs/
@@ -11,12 +10,14 @@ import lightGallery from 'lightgallery';
 // Плагины
 // lgZoom, lgAutoplay, lgComment, lgFullscreen, lgHash, lgPager, lgRotate, lgShare, lgThumbnail, lgVideo, lgMediumZoom
 // import lgThumbnail from 'lightgallery/plugins/thumbnail'
+import lgVideo from 'lightgallery/plugins/video/lg-video.es5.js';
+// import lgVideo from 'lightgallery';
 
 // Базовые стили
 import '@scss/libs/gallery/lightgallery.scss';
 // Стили дополнений
 // import '@scss/libs/gallery/lg-thumbnail.scss';
-// import '@scss/libs/gallery/lg-video.scss';
+import '@scss/libs/gallery/lg-video.scss';
 // import '@scss/libs/gallery/lg-autoplay.scss';
 // import '@scss/libs/gallery/lg-zoom.scss';
 // import '@scss/libs/gallery/lg-pager.scss';
@@ -31,18 +32,13 @@ import '@scss/libs/gallery/lightgallery.scss';
 // import '@scss/libs/gallery/lightgallery-bundle.scss';
 
 // Запуск
-const galleries = document.querySelectorAll('[data-gallery]');
+const galleries = document.querySelectorAll('.video-preview');
 if (galleries.length) {
-	galleries.forEach(gallery => {
-		lightGallery(gallery, {
-			//plugins: [lgZoom, lgThumbnail],
-			licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
-			speed: 500,
-		});
-	});
+  galleries.forEach((gallery) => {
+    lightGallery(gallery, {
+      plugins: [lgVideo],
+      licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
+      speed: 500,
+    });
+  });
 }
-
-
-
-
-
